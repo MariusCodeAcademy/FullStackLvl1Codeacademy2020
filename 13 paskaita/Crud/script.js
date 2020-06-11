e282dc8a0b4fc0b6a014e63a6215200e4261c725
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2020-06-11 20:24:47 +0300

    delete done

--- 13 paskaita/Crud/script.js
+++ 13 paskaita/Crud/script.js
@@ -12,31 +12,6 @@ const listElement = document.getElementById("list");
 const inputElement = document.getElementById("input");
 const addBtnElement = document.getElementById('add-todo-btn')
 
-// Prideti ivykiu pasiklausyma mygtukui
-addBtnElement.addEventListener('click', function() {
-    // issisaugome ivesta reiksme
-    // paimti reiksme is ivesties lauko
-    let inputVal = inputElement.value;
-    console.log({ inputVal });
-    // suurti nauja objekta is reiksmes 
-    todos.push({ name: inputVal, done: false, edit: false })
-    render();
-    inputElement.value = '';
-})
-
-// enter mygtuko funkcionalumas
-inputElement.addEventListener('keyup', function(event) {
-    // console.log(event);
-    // pasitikrinam ar buvo paspaustas enter
-    if (event.keyCode === 13) {
-        // paspaustas enter
-        let inputVal = inputElement.value;
-        // suurti nauja objekta is reiksmes 
-        todos.push({ name: inputVal, done: false, edit: false })
-        render();
-        inputElement.value = '';
-    }
-})
 
 // suformuoti li elementa ir
 function textToLi(text) {
@@ -61,7 +36,8 @@ function render() {
 
     // jei nera nei vieno todo
     if (todos.length === 0) {
-        listElement.innerHtml = `<p class="no-todo-text">There are no todos, Please add some</p>`;
+        listElement.innerHTML = `<p class="no-todo-text">There are no todos, Please add some</p>`;
+        return;
     }
 
 
@@ -71,16 +47,73 @@ function render() {
     // ikeliam sukurta struktura i html
     todos.forEach(function(todo) {
         listElement.innerHTML += `
-                                <li class="item">
+                                <li class="item" data-todo-id=${toDoId} >
                                     <i class="fa fa-circle-thin complete" aria-hidden="true"></i>
                                     <span class="text">${todo.name}</span>
                                     <i class="fa fa-pencil-square" aria-hidden="true"></i>
                                     <i class="fa fa-trash-o delete" aria-hidden="true"></i>
                                 </li>
                                 `;
+        toDoId++;
     });
 
 
 }
 
 render();
+
+// prideti nauja todo i masyva ir sugeneruoti 
+function addTodo(todoText) {
+    todos.push({ name: todoText, done: false, edit: false });
+    render();
+}
+
+// enter mygtuko funkcionalumas
+inputElement.addEventListener('keyup', function(event) {
+    // console.log(event);
+    // pasitikrinam ar buvo paspaustas enter
+    if (event.keyCode === 13) {
+        // paspaustas enter
+        let inputVal = inputElement.value;
+        // suurti nauja objekta is reiksmes 
+        addTodo(inputVal)
+        inputElement.value = '';
+    }
+})
+
+// Prideti ivykiu pasiklausyma mygtukui
+addBtnElement.addEventListener('click', function() {
+    // issisaugome ivesta reiksme
+    // paimti reiksme is ivesties lauko
+    let inputVal = inputElement.value;
+    addTodo(inputVal)
+    inputElement.value = '';
+})
+
+// istrynimas is masyvo 
+function deleteTodo(id) {
+    todos.splice(id, 1);
+    render();
+}
+
+
+
+// uzdesim event listeneri ant viso saraso ir atsirinksim ka paspaudem
+// =======================================================================
+// =======================================================================
+
+listElement.addEventListener('click', function(event) {
+    //console.log(event.target);
+    //event.target.style.backgroundColor = 'black';
+
+    // elemento istrynimas 
+    // pasitikrinam ar paspaudem ant siusklines item
+    if (event.target.classList.contains('fa-trash-o')) {
+        // console.log(event.target);
+        // gauti li elem id ant kurio buvo paspausta siukslinyte
+        let todoIdValueFromAttr = event.target.parentElement.getAttribute('data-todo-id');
+        // console.log(todoIdValueFromAttr);
+        deleteTodo(todoIdValueFromAttr);
+    }
+
+})
