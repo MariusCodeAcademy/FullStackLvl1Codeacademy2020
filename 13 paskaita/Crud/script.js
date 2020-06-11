81b274534c09f87a748a74b71904e9d26f2d93ef
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2020-06-11 19:54:09 +0300

    last

--- 13 paskaita/Crud/script.js
+++ 13 paskaita/Crud/script.js
@@ -12,7 +12,6 @@ const listElement = document.getElementById("list");
 const inputElement = document.getElementById("input");
 const addBtnElement = document.getElementById('add-todo-btn')
 
-
 // Prideti ivykiu pasiklausyma mygtukui
 addBtnElement.addEventListener('click', function() {
     // issisaugome ivesta reiksme
@@ -25,10 +24,21 @@ addBtnElement.addEventListener('click', function() {
     inputElement.value = '';
 })
 
-
+// enter mygtuko funkcionalumas
+inputElement.addEventListener('keyup', function(event) {
+    // console.log(event);
+    // pasitikrinam ar buvo paspaustas enter
+    if (event.keyCode === 13) {
+        // paspaustas enter
+        let inputVal = inputElement.value;
+        // suurti nauja objekta is reiksmes 
+        todos.push({ name: inputVal, done: false, edit: false })
+        render();
+        inputElement.value = '';
+    }
+})
 
 // suformuoti li elementa ir
-
 function textToLi(text) {
     let result = `
     <li class="item">
@@ -40,25 +50,37 @@ function textToLi(text) {
     return result
 }
 
-
-
-// console.log(textToLi("Go for a Walk"));
-
 //  ikelti ji i dom 
 function addElToList(el) {
     listElement.innerHTML = el;
 }
 
-
+// pagrindine generavimo funkcija sugeneruoti li el is masyvo
 function render() {
     listElement.innerHTML = '';
-    let output = ''
-        // paimame name reikskmes is todos masyvo ir sukuriam li HTML text
-    todos.forEach(function(todo) {
-        output += textToLi(todo.name)
-    });
+
+    // jei nera nei vieno todo
+    if (todos.length === 0) {
+        listElement.innerHtml = `<p class="no-todo-text">There are no todos, Please add some</p>`;
+    }
+
+
+
+    let toDoId = 0;
+    // paimame name reikskmes is todos masyvo ir sukuriam li HTML text
     // ikeliam sukurta struktura i html
-    addElToList(output);
+    todos.forEach(function(todo) {
+        listElement.innerHTML += `
+                                <li class="item">
+                                    <i class="fa fa-circle-thin complete" aria-hidden="true"></i>
+                                    <span class="text">${todo.name}</span>
+                                    <i class="fa fa-pencil-square" aria-hidden="true"></i>
+                                    <i class="fa fa-trash-o delete" aria-hidden="true"></i>
+                                </li>
+                                `;
+    });
+
+
 }
 
 render();
