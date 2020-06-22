fb34b4fa0fd673249f925dd913385c34e69cc213
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2020-06-22 19:15:53 +0300

    Add files via upload

--- 16pask/formos_test/index.php
+++ 16pask/formos_test/index.php
@@ -19,11 +19,33 @@
     <h2>Post</h2>
 
     <form action="process.php" method="post">
-        Vardas: <input type="text" name="vardas" placeholder="Iveskite varda"> <br><br>
-        El pastas: <input type="email" name='pastas' placeholder="Iveskeite el pasto adresa"><br><br>
-        Slaptas: <input type="password" name='slaptas' placeholder="Iveskeite slaptazodis"><br><br>
+        Vardas:     <input type="text" name="vardas" placeholder="Iveskite varda"> <br><br>
+        El pastas:  <input required type="email" name='pastas' placeholder="Iveskeite el pasto adresa"><br><br>
+        Slaptas:    <input required minlength="4" type="password" name='slaptas' placeholder="Iveskeite slaptazodis"><br><br>
         <button type="submit">Siusti duomenis</button>
     </form>
+
+
+    <!-- 
+        » Sukurkite formą su GET metodu, kurioje būtų
+        skaičiuojama dviejų skaičių daugyba.
+        » Sukurkite POST registracijos formą su
+        pakartotiniu slaptažodžiu ir būtinais duomenimis.
+        Registracijos duomenis išsiųskite į kitą
+        registracija.php failą ir juos atvaizduokite. -->
+
+        <p>» Sukurkite formą su GET metodu, kurioje būtų
+        skaičiuojama dviejų skaičių daugyba.</p>
+
+    <form action="process.php">
+        <input type="number" name="skaic1" id="" placeholder="iveskite prima skaiciu">
+        <input type="number" name="skaic2" id="" placeholder="iveskite antra skaiciu"><br>
+        <input type="text" name="pranesimas" id="" placeholder="pranesimas">
+        <button type="submit"> skaiciuoti</button>
+    </form>
+
+    
+    <!-- <script>alert('Laba diena');</script> -->
     
 </body>
 </html>
--- 16pask/formos_test/process.php
+++ 16pask/formos_test/process.php
@@ -5,13 +5,36 @@
     // jei nustatytas ir netuscias GET masyvas tai ji atspausdinti 
     if(isset($_GET) && !empty($_GET)) {
         echo '$_GET <br>';
-        print_r($_GET);
+        //print_r($_GET);
 
         if(isset($_GET['vardas'])) {
             $VartotojoVardas = $_GET['vardas'];
             echo $VartotojoVardas;
         }
     }
+
+    // pirma uzduotis 
+    // » Sukurkite formą su GET metodu, kurioje būtų
+    // skaičiuojama dviejų skaičių daugyba.
+
+    // gaunam 1 reiksme
+    $num1 = $_GET['skaic1'];
+
+    // gaunam antra reiksme
+    $num2 = $_GET['skaic2'];
+
+    // atspausdinit daugyba
+
+    echo 'Dvieju ivestu skaiciu daugyba yra lygi: ' . ($num1 * $num2);
+
+
+    echo '<br>';
+    //nesaugu reikia issivalyti ivesties lauka nuo pavojingu simboliu 
+    // echo $_GET['pranesimas']; 
+    // saugus html simboliu isvalymas
+    echo htmlspecialchars($_GET['pranesimas']);
+
+
 
     echo '</pre>';
 ?>
