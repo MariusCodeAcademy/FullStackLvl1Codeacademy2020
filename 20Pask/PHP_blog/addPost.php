bbddd77da28c6ebbb80a4b3c497aca56bc4eb4e8
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2020-07-13 21:20:09 +0300

    Add files via upload

--- 20Pask/PHP_blog/addPost.php
+++ 20Pask/PHP_blog/addPost.php
@@ -16,6 +16,31 @@ include('./inc/navigation.php');
 <section class="container">
     <h1 class="display-3 my-5">Add new Post</h1>
     
+    <!-- Forma ikelti postui -->
+    <form class="w-50" action="addPost.php">
+        <!-- title -->
+        <div class="form-group">
+            <label for="">Title</label>
+            <input type="text" class="form-control" name='title'>
+        </div>
+
+        <!-- author -->
+        <div class="form-group">
+            <label for="">Autorius</label>
+            <input type="text" class="form-control" name='author'>
+        </div>
+
+        <!-- post body -->
+        <div class="form-group">
+            <label for="">Textas</label>
+            <textarea class="form-control" name="body" cols="30" rows="10"></textarea>
+        </div>
+
+        <!-- siuntimo mygtuas -->
+        <button class="btn btn-lg btn-primary" name='submit' type="submit">Siusti</button>
+
+    </form>
+    <!-- /Forma ikelti postui -->
 
 
 
